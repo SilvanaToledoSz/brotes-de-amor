8b10ad9e7473cb04602b3b42ceef15925b8ad3b4
SilvanaToledoSz <silvana.toledo.sz@gmail.com>
2022-06-11 22:10:01 -0300

    carga primera entrega proyecto final y segundo desafio complementario

--- js/objetos.js
+++ js/objetos.js
@@ -1,14 +1,29 @@
 class Planta {
-    constructor(nombre, valorBruto, alto, ancho, peso, stock) {
+    constructor(nombre, valorBruto, alto, ancho, peso, stock, precioUnidades) {
         this.nombre = nombre.toUpperCase();
         this.valorBruto = parseFloat(valorBruto);
         this.alto = parseFloat(alto);
         this.ancho = parseFloat(ancho);
         this.peso = parseFloat(peso);
         this.stock = parseInt(stock);
-        this.vendido = false;
-        
+        this.vendido = false;       
+        this.precioUnidades = parseInt(precioUnidades);
+    }    
+
+    valorIva() {
+        this.valorBruto = parseInt(this.valorBruto * 1.21)
+    }   
+    productoVendido() {
+        this.vendido = true;
     }
-        
+
 }
+
+
+
+
+
+
+
+
 
--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,25 @@
-const planta1 = new Planta("rosal", 1200.0, 1.1, 0.30, 7.5, 13);
-const planta2 = new Planta("begonia", 750.0, 0.3, 0.25, 1.2, 25);
-const planta3 = new Planta("malvon", 525.0, 0.5, 0.5, 1.1, 10);
-const planta4 = new Planta("ficus", 2200, 1.5, 0.9, 4.5, 1);
-const planta5 = new Planta("rosario bananita", 450, 0.45, 0.15, 2.25, 0);
-const planta6 = new Planta("cactus espiralado", 250, 0.15, 0.07, 0.3, 0);
+// Implementación de array de objetos
 
+const plantas = []
+plantas.push (new Planta ("rosal", 1200.0, 1.1, 0.30, 7.5, 13, 0, 0))
+plantas.push (new Planta ("begonia", 750.0, 0.3, 0.25, 1.2, 25, 0, 0))
+plantas.push (new Planta ("malvon", 525.0, 0.5, 0.5, 1.1, 10, 0, 0))
+plantas.push (new Planta ("ficus", 2200, 1.5, 0.9, 4.5, 1, 0, 0))
+plantas.push (new Planta ("rosario bananita", 450, 0.45, 0.15, 2.25, 0, 0, 0))
+plantas.push (new Planta ("cactus espiralado", 250, 0.15, 0.07, 0.3, 0, 0, 0))
+
+const carritoFinal = []
+
+// otras variables
+
+let precioIva = 0
 let valorEnvio = 650
 let direccionLocal = "Calle falsa 123, Tierra del Fuego, Argentina."
 let porcentajeMP = 1.35
+let productoUnidades = 0
+let envioDom = 0
+let totalFinal = 0
+
+// otras funciones sobre el array de objetos
+
+const precioFinal = (a,b) => a + b
